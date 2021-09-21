cac769ce76b52718fc233a050140e13d0a0079f4
“User <musagurbuz4661@gmail.com>
2021-09-21 19:47:13 +0200

    AppRouter.js

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import Navbar from './components/navbar/Navbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import About from './pages/About';
+import Html from './pages/Html';
+import Css from './pages/Css';
+import Logo from './pages/Logo';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 const AppRouter = () => {
     return (
         <Router>
-            <Navbar/>
-
-            
-           
-
+            <Navbar />
+            <Switch>
+                <Route path="/services" component={Services} />
+                <Route path="/about" component={About} />
+                <Route path="/html" component={Html} />
+                <Route path="/css" component={Css} />
+                <Route path="/logo" component={Logo} />
+                <Route path="/" component={Home} />
+            </Switch>
+            <Footer />
         </Router>
     )
 }
