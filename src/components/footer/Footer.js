9e9dfc7b0af30b2414b0d1011fb250ee502e8696
“User <musagurbuz4661@gmail.com>
2021-09-21 19:54:30 +0200

    Footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../../App.css'
 
 const Footer = () => {
     return (
         <div>
-            
+            <footer>
+                <p>Clarusway Web Desing, copyright &copy; 2021</p>
+                <br />
+                <Link to={{ pathname: 'http://www.clarusway.com' }} target="_blank">
+                    <img
+                        src="https://clarusway.com/wp-content/uploads/2020/06/clarusway_logo.png" width="200"
+                        height="40"
+                        alt="" />
+                </Link>
+            </footer>
         </div>
     )
 }
